47f26302f982d691837e1ae4830514a2ead7aaed
HSP <40883035+Hspharwinder@users.noreply.github.com>
2020-10-11 20:28:30 +0530

    Update chatUser.js

--- node js project/schema/chatUser.js
+++ node js project/schema/chatUser.js
@@ -1,6 +1,4 @@
 var mongoose = require('mongoose');
-// var autoIncrement = require('mongoose-auto-increment');
-// var ObjectId = require('mongoose').ObjectId;
 
 var chatUser = new mongoose.Schema({
     userId: { type : String},
@@ -8,8 +6,4 @@ var chatUser = new mongoose.Schema({
     userCreatedAt: { type: Date } 
 });
 
-// mongoose.set('useCreateIndex', true); // for remove (node:11052) DeprecationWarning: collection.ensureIndex is deprecated. Use createIndexes instead.
-// autoIncrement.initialize(mongoose.connection);
-
-// wishListSchema.plugin(autoIncrement.plugin, 'wishList');
 mongoose.model('chatUser', chatUser);
